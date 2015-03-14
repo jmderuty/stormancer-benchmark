e45b433b0b9eff495a4e2fa51af7a1ab64787879
jmderuty <jmderuty@stormancer.com>
2015-03-14 14:46:51 +0100

    added server code

---
+++ BenchmarkServer/App.cs
@@ -0,0 +1,34 @@
+﻿using Stormancer;
+using Stormancer.Core;
+using Stormancer.Server;
+using System;
+using System.Collections.Generic;
+
+namespace Benchmark
+{
+    public class App : IStartup
+    {
+
+        public void Run(IAppBuilder builder)
+        {
+            builder.SceneTemplate("test-template", scene =>
+            {
+                scene.AddRoute("echo.in", p =>
+                {
+                    var msg = p.ReadObject<Msg>();
+                    //msg.SenderId = 0;
+                    scene.Broadcast("echo.out", s => p.Serializer().Serialize(msg,s), PacketPriority.MEDIUM_PRIORITY, PacketReliability.UNRELIABLE);
+                });
+            },
+            new Dictionary<string, string> { { "description", "Broadcasts data sent to the route 'echo.in' to all connected users on the route 'echo.out'." } });
+        }
+    }
+
+    public class Msg
+    {
+        public long SenderId { get; set; }
+
+        public long RequestId { get; set; }
+
+    }
+}
