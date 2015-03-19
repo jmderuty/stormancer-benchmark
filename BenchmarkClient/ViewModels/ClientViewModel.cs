1b3cabb42a3a0986b13bdb8acb6ce9157ef12ce5
jmderuty <jmderuty@stormancer.com>
2015-03-19 11:12:19 +0100

    added viewmodels to project

---
+++ BenchmarkClient/ViewModels/ClientViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarkClient.ViewModels
+{
+    class ClientViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private readonly ObservableCollection<Metric> _metric = new ObservableCollection<Metric>();
+
+        public void AddMetric(Metric metric)
+        {
+            Metrics.Add(metric);
+            LastValue = metric;
+        }
+
+        private Metric _lastValue;
+        public Metric LastValue
+        {
+            get
+            {
+                return _lastValue;
+            }
+            set
+            {
+                if(value != _lastValue)
+                {
+                    _lastValue = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public ObservableCollection<Metric> Metrics
+        {
+            get
+            {
+                return _metric;
+            }
+        }
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var propertyChanged = PropertyChanged;
+            if (propertyChanged != null)
+            {
+                propertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+    class Metric
+    {
+        public double Min { get; set; }
+
+        public double Max { get; set; }
+
+        public double Avg { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
---
+++ BenchmarkClient/ViewModels/TestRunViewModel.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace BenchmarkClient.ViewModels
+{
+    class TestRunViewModel
+    {
+        private Dispatcher _d;
+        public async Task StartTest(int nbClients, object clientConfig, CancellationToken token)
+        {
+            _d = App.Current.Dispatcher;
+            var json = JsonConvert.SerializeObject(clientConfig);
+            var args = Convert.ToBase64String(Encoding.UTF8.GetBytes(json));
+            var tasks = new List<Task>();
+            for (int i = 0; i < nbClients; i++)
+            {
+                var psInfos = new ProcessStartInfo("worker/benchmark.worker.exe", args);
+                psInfos.CreateNoWindow = true;
+                psInfos.UseShellExecute = false;
+                psInfos.RedirectStandardOutput = true;
+                var prc = Process.Start(psInfos);
+                tasks.Add(ConnectToWorker(prc, token));
+                await Task.Delay(2000);
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
+        private Task ConnectToWorker(Process prc, CancellationToken token)
+        {
+            return Task.Run(async () =>
+            {
+                var client = new ClientViewModel();
+                _d.Invoke(() => { 
+                    Clients.Add(client); 
+                });
+                try
+                {
+                    while (!token.IsCancellationRequested)
+                    {
+                        var json = await prc.StandardOutput.ReadLineAsync();
+                        var metric = JsonConvert.DeserializeObject<Metric>(json);
+                        metric.Date = DateTime.Now;
+                        _d.Invoke(() => { client.AddMetric(metric); });
+
+                    }
+                }
+                finally
+                {
+                    prc.Kill();
+                    _d.Invoke(() => { Clients.Remove(client); });
+                }
+            });
+
+        }
+        private readonly ObservableCollection<ClientViewModel> _clients = new ObservableCollection<ClientViewModel>();
+        public ObservableCollection<ClientViewModel> Clients
+        {
+            get
+            {
+                return _clients;
+            }
+        }
+    }
+}
