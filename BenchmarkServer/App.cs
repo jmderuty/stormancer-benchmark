6f70d904fc23074ecabfba4054bb9108735ad5ae
Nia.elfette@live.fr <Nia.elfette@live.fr>
2015-03-20 13:12:43 +0100

    message aggregation for broadcast

--- BenchmarkServer/App.cs
+++ BenchmarkServer/App.cs
@@ -2,7 +2,10 @@
 using Stormancer.Core;
 using Stormancer.Server;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Benchmark
 {
@@ -10,12 +13,47 @@ namespace Benchmark
     {
         public void Run(IAppBuilder builder)
         {
+
             builder.SceneTemplate("test-template", scene =>
                 {
-                    scene.AddRoute("broadcast.in", p =>
+                    var msgQueue = new ConcurrentQueue<Stream>();
+                    var sceneRunning = true;
+                    scene.Starting.Add(data =>
                     {
 
-                        scene.Broadcast("broadcast.out", s => p.Stream.CopyTo(s), PacketPriority.MEDIUM_PRIORITY, PacketReliability.UNRELIABLE);
+                        var t = Task.Run(async () =>
+                        {
+
+                            while (sceneRunning)
+                            {
+                               
+                                scene.Broadcast("broadcast.out", output =>
+                                {
+                                    Stream s;
+                                    while (msgQueue.TryDequeue(out s))
+                                    {
+                                        s.CopyTo(output);
+                                    }
+                                   
+                                }, PacketPriority.MEDIUM_PRIORITY, PacketReliability.UNRELIABLE);
+
+                                await Task.Delay(30);
+                              
+                            }
+
+                        });
+
+                        return Task.FromResult(true);
+                    });
+                    scene.Shuttingdown.Add(ctx =>
+                    {
+                        sceneRunning = false;
+                        return Task.FromResult(true);
+                    });
+                    scene.AddRoute("broadcast.in", p =>
+                    {
+                        msgQueue.Enqueue(p.Stream);
+                        //scene.Broadcast("broadcast.out", s => p.Stream.CopyTo(s), PacketPriority.MEDIUM_PRIORITY, PacketReliability.UNRELIABLE);
                     });
 
                     scene.AddRoute("echo.in", p =>
@@ -27,7 +65,7 @@ namespace Benchmark
                 new Dictionary<string, string> { { "description", "Broadcasts data sent to the route 'broadcast.in' to 'broadcast.out' and echoes data sent to 'echo.in' to 'echo.out' " } }
             );
 
-            
+
         }
     }
 
